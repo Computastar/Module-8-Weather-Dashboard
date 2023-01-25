d54431da57c813e2233387f13e2f7842add58fc6
Computastar <jason.hunter@computastar.com>
2023-01-25 18:44:53 +0000

    create a span element to for temperature data in displayWeather

--- starter/assets/scripts/index.js
+++ starter/assets/scripts/index.js
@@ -1,11 +1,12 @@
 let cityFormElement=document.querySelector("#city-search-form");
 let cityInputElement=document.querySelector("#city");
 let citySearchInputEl = document.querySelector("#searched-city");
+let weatherContainerEl=document.querySelector("#current-weather-block");
 
 const getCityWeather = function(city){
     if(city){
       const apiKey = "2726ba0963847f452e627fcd003602d0"
-      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+      const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
   
       fetch(apiURL)
       .then(function(response){
@@ -27,12 +28,21 @@ let displayWeather = function (weatherData, city) {
 
   //create an image element for icon displayed
   let weatherIcon = document.createElement("img");
-
   weatherIcon.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
   );
   citySearchInputEl.appendChild(weatherIcon);
+
+  //create a span element to for temperature data
+  let tempElement = document.createElement("span");
+  tempElement.textContent = "Current Temp: " + weatherData.main.temp + " °C";
+  tempElement.classList = "list-group-item";
+
+     //append to temp container
+     weatherContainerEl.appendChild(tempElement);
+
+  
 };
 
   var formSumbitAction = function(event){
